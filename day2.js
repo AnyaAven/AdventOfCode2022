ad40f1a87c38fcbba5fd7c2daef2d5fe61d1a9cb
AnyaAven <leafeon96@gmail.com>
2024-03-06 19:22:57 -0500

    rename

--- day2.js
+++ day2.js
@@ -5,7 +5,7 @@ function readLinesFromTxt(txtFilePath) {
     const txtToString = txt.toString()
     return txtToString.split('\n')
 } //reading a text file
-const puzzInputTxtFilePath = "./day1Input.txt"
+const puzzInputTxtFilePath = "./day2-PuzzInput.txt"
 const puzzInput = readLinesFromTxt(puzzInputTxtFilePath)
 
 console.log(puzzInput)
